91f66e7aa71f396d5439f2951b52b73f05a9fdcb
Ben Wiley <therealbenwiley@gmail.com>
2015-04-20 15:40:32 -0400

    Classes added for Node and Ant.

---
+++ node.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+"""Code for Node class of Ant-Based Load Balancing."""
+
+""" imports here? """
+
+__author__ = "Ben Wiley and Tommy Rhodes"
+__email__ = "bewiley@davidson.edu, torhodes@davidson.edu"
+
