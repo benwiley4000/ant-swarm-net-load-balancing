5e7485570a9c8e76dd08d6dadebc635ef239fd7d
Ben Wiley <therealbenwiley@gmail.com>
2015-04-20 15:34:29 -0400

    Objects sketch added.

---
+++ client.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+"""Client code for Artificial Intelligence Final (Ant-Based Load Balancing)."""
+
+""" imports here? """
+
+__author__ = "Ben Wiley and Tommy Rhodes"
+__email__ = "bewiley@davidson.edu, torhodes@davidson.edu"
+
+def main():
+    
+    
