33e0d730be2b38257dce1361b7263d4f37286ee8
Tommy Rhodes <tommy.rhodes97@gmail.com>
2015-04-23 22:19:35 -0400

    Update client.py

--- client.py
+++ client.py
@@ -3,23 +3,38 @@
 #import node
 import random
 
-"""Client code for Artificial Intelligence Final (Ant-Based Load Balancing)."""
+"""Client code for Artificial Intelligence Final (Ant-Based Load Balancing)"""
 
 __author__ = "Ben Wiley and Tommy Rhodes"
 __email__ = "bewiley@davidson.edu, torhodes@davidson.edu"
 
-def send_ant(noise, p_table, age):
+def send_ant(noise, p_table, dest):
     random.seed()
     rand = random.random()
     if rand >= noise: 
-        #select random node
+        next_node = int(random.randrange(len(
         return
     rand = random.random()
     #select node from values in p_table
     #update p_table
     return
 
+def route_call(source, dest, adj_list):
+    current_node = adj_list[source]
+    nodes = []
+    while current_node.num != dest:
+        current_node = adj_list[current_node.neighbors[current_node.p_table[current_node.num].max(current_node.p_table[current_node.num])]] #def recheck this later
+        if current_node.max_load - current_load.load == 0:
+            for i in range(len(nodes)):
+                adj_list[i].load -= 1
+            return -1
+        nodes.append(current_node.num)
+        current_node.load += 1
+    return [source, dest]
+
 def main():
+    noise = .05
+    random.seed()
     adj_list = []
     #initialize graph
     #iterate through initialization and nodes
@@ -28,8 +43,12 @@ def main():
     call_list = []
     successful_call_list = []
     lost_call_list = []
-    #iterate
+    for ("""iterate"""):
         #randomly add calls
-        #send_ant(noise prob, node.p_table, ant.age + length)
+        if random.random() >= .9:
+            a = int(random.randrange(len(adj_list)) + 1)
+            b = int(random.randrange(len(adj_list)) + 1)
+            call = route_call(a, b, adj_list)
+        #send_ant(noise, node.p_table, dest)
         #track lost calls
         #when calls end, add to successful_call_list
